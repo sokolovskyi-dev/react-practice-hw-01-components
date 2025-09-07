195d98b9c115cef2bca2767d9234c9f0947b50be
DevMasterX <sokol080288@gmail.com>
2025-09-07 20:33:02 +0300

    8709

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './Profile/Profile';
 import user from '../user.json';
 import data from '../data.json';
+import transactions from '../transactions.json';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendLList';
 import friends from '../friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -29,8 +31,8 @@ export const App = () => {
       />
 
       <Statistics title="Upload stats" stats={data} />
-
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
